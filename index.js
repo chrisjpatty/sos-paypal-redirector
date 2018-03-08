b0deece7f6edd366bf21c9a9cd82a88a73b421a6
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2018-03-08 14:31:25 -0700

    Removed parcel and opted for Express server

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-import parseURLQuery from 'decode-query-string'
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var http = require('http').Server(app);
 
-const params = parseURLQuery(window.location.search);
-const description = params.DESCRIPTION
-const origin = description.split('|||')[1].split('=')[1]
-console.log(origin);
-window.location.replace(origin + window.location.search)
+app.use(cors())
+
+app.post('/silent', function (req, res, next) {
+  const query = req.query;
+  const customParams = JSON.parse(query.USER1)
+  console.log('silent', query);
+  res.send('Hit Silent')
+})
+
+app.get('/success', function (req, res, next) {
+  const query = req.query;
+  const customParams = JSON.parse(query.USER1)
+  console.log('success', query);
+  res.send(`${customParams.ORIGIN}/paymenterror?SECURETOKENID=${query.SECURETOKENID}`)
+})
+
+app.get('/error', function (req, res, next) {
+  const query = req.query;
+  const customParams = JSON.parse(query.USER1)
+  console.log('error', query);
+  res.redirect(`${customParams.ORIGIN}/paymenterror?SECURETOKENID=${query.SECURETOKENID}`)
+})
+
+http.listen(process.env.PORT || 8080, "0.0.0.0", () => {
+  console.log("Listening on: ", process.env.PORT || 8080)
+})
