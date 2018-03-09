dd6b98b08ea2369f081b09fc83df5b80a36a7d09
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2018-03-09 16:39:33 -0700

    Added request headers

--- index.js
+++ index.js
@@ -31,9 +31,13 @@ app.post('/paypal/silent', function (req, res, next) {
   // const apiUrl = customParams.ENV === 'development' ? 'http://192.168.111.57:53013' : customParams.ORIGIN + '/api'
   fetch(`http://192.168.111.57:3001/api/Payment/silent`, {
     method: 'POST',
-    body: req.body
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(req.body)
   })
-  .then(() => {
+  .then(response => {
+    console.log(response, response.ok);
     res.send(true)
   })
   .catch(err => {
