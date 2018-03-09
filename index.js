df191a0a3ca569f10f49bc48b70cae8106d84c42
Christopher Patty <christopher.patty@aggiemail.usu.edu>
2018-03-09 15:27:18 -0700

    stringified query as body

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.post('/paypal/test', function (req, res, next) {
 })
 
 app.post('/paypal/silent', function (req, res, next) {
-  // const query = req.query;
+  const query = req.query;
   // const customParams = JSON.parse(query.USER1)
   // const apiUrl = customParams.ENV === 'development' ? 'http://192.168.111.57:53013' : customParams.ORIGIN + '/api'
   fetch(`http://192.168.111.57:3001/api/Payment/silent`, {
     method: 'POST',
-    body: req.body
+    body: JSON.stringify(query)
   })
   .then(() => {
     res.send(true)
